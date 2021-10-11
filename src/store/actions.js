3c487cdf7099c0f42839a08f2b2ce81fd1c69db9
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-10-11 14:00:28 +0300

    save

---
+++ src/store/actions.js
@@ -0,0 +1,9 @@
+import { DATA } from "../data"
+import { LOAD_POSTS, TOGGLE_BOOKED } from "./types"
+
+export const LoadPosts = () => {
+	return { type: LOAD_POSTS, payload: DATA }
+}
+export const TogglBooked = (id) => {
+	return { type: TOGGLE_BOOKED, payload: id }
+}
